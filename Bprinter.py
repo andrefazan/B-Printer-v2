2c6d42c46f178d91c5cb5b6af7a9e24510349dc3
andrefazan <75899235+andrefazan@users.noreply.github.com>
2021-11-03 18:17:10 -0300

    add

--- Bprinter.py
+++ Bprinter.py
@@ -13,7 +13,7 @@ from tkinter import messagebox
 import sys
 
 
-#count test threading in webdriver
+#using a list cause that can be changed in functions 
 counter = [0]
 new_driver = 'driver'
 
@@ -21,17 +21,15 @@ new_driver = 'driver'
 options = webdriver.ChromeOptions()
 options.headless = True
 
-
 #using sys to get the path to the backup folder location txt file
 location_backup_txt = '{}/backup-folder-location.txt'.format(sys.path[0])
-
 
 # reading folder directory backup
 with open(location_backup_txt, 'r') as f:
         folder_location_program = f.readline() 
 f.close()
 
-# ask for folder location and change it in the entry. Also create a backup for it.
+# ask for folder location and change it in the entry. Also changed in the backup.
 def get_the_folder_location():
     folder_location_program = filedialog.askdirectory()
     label_where_to_save.delete(0, END)
@@ -40,51 +38,66 @@ def get_the_folder_location():
         f.write(folder_location_program)  
     f.close()
 
-def test_thread():
-    print(0)
-    time.sleep(1)
-    print(1)
-    time.sleep(1)
-    print(2)
-    time.sleep(1)
-    print(3)
-    time.sleep(1)
-    print(4)
-    time.sleep(1)
-    print(5)
-
-
-
-def add_more_one(new_driver):
-    return new_driver + 1
-
+#change the new_driver name for a new use
 def change_new_driver(new_driver):
-    global counter
-    return new_driver + str(len(counter))
+    return new_driver + str(1)
 
-
+def change_number_variable(number_variable):
+    return(number_variable + str(len(counter)))
+    
 #screenshot function
 def make_a_full_screenshot(counter): 
     global new_driver
 
-    counter.append(1)
+    #check if is the first print, to create the folder
+    if len(counter) == 1:
+        #create new folder
+        create_new_folder()
+
     new_driver = change_new_driver(new_driver)     
     print(new_driver)   
     make_print_and_close_driver(new_driver)
 
-        
+def create_new_folder():
+    local_to_save = label_where_to_save.get()
+    name_folder = entry_folder_name.get()
+    path_new_folder = '{}/{}'.format(local_to_save,name_folder)
+    os.makedirs(path_new_folder)
+    counter.append(1)
+    
+
+def make_print_and_close_driver(driver):    
 
 
-def make_print_and_close_driver(driver):
+    print('start')
+     
+    number_variable = int(len(counter)-2)
+    #number_variable = change_number_variable(number_variable)
+
+    make_print_and_close_driver_2(number_variable)
+
+def make_print_and_close_driver_2(number):
+    print(number)
+    local_to_save = label_where_to_save.get()
+    name_folder = entry_folder_name.get()
+    url = entry_url_print.get()
+    name_print = entry_name_print.get()
+
+    #delete entry values (name and url)
+    entry_url_print.delete(0, END)
+    entry_name_print.delete(0, END)
+
+    #label_infos.insert(number, str('GENERATING :{}'.format(name_print))) 
+
+
     driver = webdriver.Chrome(ChromeDriverManager().install(), options= options)             
-    driver.get('https://www.emkt.cantasys.com.br/dados/16/emkts/37899/v7/')      
-    link = (r'C:\Users\andre\Desktop\test.png')
+    driver.get(url)
     S = lambda X: driver.execute_script('return document.body.parentNode.scroll'+X)
     driver.set_window_size(650, S('Height'))
+    path_print = ('{}/{}/{}.png'.format(local_to_save,name_folder,name_print))       
 
     #function that ensures all images are loaded
     number_of_images_in_the_website = driver.execute_script('return document.images.length')
-
     images_loading = True
     while images_loading == True:         
         need_wait = False     
@@ -94,17 +107,16 @@ def make_print_and_close_driver(driver):
                 need_wait = True
 
         if need_wait == False:
-            driver.find_element_by_tag_name('body').screenshot(link)
+            driver.find_element_by_tag_name('body').screenshot(path_print)
             driver.quit()
+            print('finished ')
+
+            #label_infos.delete(number, END)
+            label_infos.insert(number, str('OK {}'.format(name_print)))     
            
 
 def start_new_thread():
     threading.Thread(target= make_a_full_screenshot, args=[counter]).start()
-
-
-
-
-
 
 # starting interface
 root = Tk()
@@ -119,6 +131,11 @@ base_img_logo = os.path.dirname(__file__)
 logo_img_path = os.path.join(base_img_logo, 'new-logo-img.png')
 logo_img = PhotoImage (file = logo_img_path)
 label_logo_img = Label(root, image= logo_img, compound='top' ).grid(row=0, column=1, rowspan=2)
+#line 0 second image
+base_img_logo2 = os.path.dirname(__file__)
+logo_img_path2 = os.path.join(base_img_logo2, 'old-logo-img.png')
+logo_img2 = PhotoImage (file = logo_img_path2)
+label_logo_img2 = Label(root, image= logo_img2, compound='top' ).grid(row=0, column=3, rowspan=2, sticky='w')
 #line 1
 base_img_title = os.path.dirname(__file__)
 logo_img_title_path = os.path.join(base_img_title, 'title.png')
@@ -135,9 +152,9 @@ label_space_row_4 = Label(root, text ='').grid(row=4, column=0)
 #line 4
 label_folder_name = Label(root, text ='Folder').grid(row=5, column=1)
 #line 6
-text_prints_prog = Entry(root, width=17)
-text_prints_prog.grid(row=6, column=1)
-text_prints_prog.insert(INSERT, 'PRINTS-PROG-V')
+entry_folder_name = Entry(root, width=17)
+entry_folder_name.grid(row=6, column=1)
+entry_folder_name.insert(INSERT, 'PRINTS-PROG-V')
 #_row 6 column 2
 check_button_desk = Checkbutton(text='Screenshot Desk', bg ="gray80")
 check_button_desk.grid(row=6, column=2)
@@ -149,30 +166,20 @@ label_space_row_7 = Label(root, text ='').grid(row=7, column=0)
 label_print_name = Label(root, text ='Name').grid(row=8, column=1)
 label_url = Label(root, text ='URL').grid(row=8, column=2)
 #line 9
-text_prints_prog = Entry(root, width=15).grid(row=9, column=1)
-text_prints_prog = Entry(root, width=30).grid(row=9, column=2)
+entry_name_print = Entry(root, width=15)
+entry_name_print.grid(row=9, column=1)
+entry_url_print = Entry(root, width=30)
+entry_url_print.grid(row=9, column=2)
 #line 10
 text_prints_prog = Button(root, text='RUN', width=10,  command=start_new_thread).grid(row=9, column=3, sticky='w')
 label_space_row_10 = Label(root, text ='').grid(row=10, column=0)
 #line 11
 label_infos = Listbox(root, height=8, width=50, fg="blue4", bg ="gray80")
 label_infos.grid(row= 11, column=1, columnspan=2, sticky='w')
-button_zip_file = Button(root, text='Zip Files', command= lambda: make_a_full_screenshot(counter))
+button_zip_file = Button(root, text='Zip Files')
 button_zip_file.grid(row=11, column=3)
 #button_zip_file = Button(root, text='Zip Files', command=make_a_full_screenshot).grid(row=11, column=3)
 #line 12
 label_version_text = Label(root, text='version 2.2').grid(row= 12, column=0)
-
-
-
-#label_infos.insert(1, 'PRINTS-PROG-Vaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
-#label_infos.insert(2, 'PRINTS-PROG-Vaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
-#label_infos.insert(3, 'PRINTS-PROG-Vaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
-#label_infos.insert(4, 'PRINTS-PROG-Vaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
-#label_infos.insert(5, 'PRINTS-PROG-Vaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
-#label_infos.insert(6, 'PRINTS-PROG-Vaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
-#label_infos.insert(7, 'PRINTS-PROG-Vaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
-#label_infos.insert(8, 'PRINTS-PROG-Vaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
-#label_infos.insert(9, 'PRINTS-PROG-Vaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
 
 root.mainloop()
